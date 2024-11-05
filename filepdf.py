95bd8207cd29cd202e8634778f69919618c370f9
Prem-sharma34 <ps4183085@gmail.com>
2024-11-05 10:10:50 +0530

    Some starter code

---
+++ filepdf.py
@@ -0,0 +1,13 @@
+from pdf2docx import Converter
+
+pdf_file = 'check.pdf'
+docx_file = 'check.docx'  # Use the correct variable name
+
+# Create a Converter object
+cv = Converter(pdf_file)
+
+# Convert the PDF to DOCX
+cv.convert(docx_file)
+
+# Close the Converter object
+cv.close()
